7e6f2214add462feb467fc4e506008c412393ec1
Huynh Le Quoc Bao <huynhlequocbao2001@gmail.com>
2022-06-30 15:59:50 +0700

    commit

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,13 @@
+class NewController {
+  
+  index(req, res) {
+    res.render('news')
+  }
+
+  show(req, res) {
+    res.send('NEW DETAILS!')
+  }
+
+}
+
+module.exports = new NewController;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars')
 const path = require('path')
 const app = express()
 const port = 3000
+const route = require('./routes/index.js')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({
@@ -21,21 +22,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  res.send('');
-})
+//Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
